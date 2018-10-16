870df484cc49e70e277f96dff308694939033bea
Francesco Stabile <stabile.francesco99@gmail.com>
2018-10-16 21:28:51 +0200

    First Screen Complete

--- Iter/ViewController.swift
+++ Iter/ViewController.swift
@@ -22,33 +22,8 @@ class ViewController: UIViewController, UITableViewDelegate , UITableViewDataSou
     
     @IBOutlet weak var searchBar: UISearchBar!
     
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return(ArrayCity.count)
-//    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 110
-    }
 
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        /*let cells=UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "CityCell")
-        cells.textLabel?.text=listcity[indexPath.row]*/
-        if searching {
-            let cells=Bundle.main.loadNibNamed("CellsBtn", owner: self, options: nil)?.first as! CellsBtn
-            cells.BtnCities.setTitle(searchCity[indexPath.row].City, for:UIControl.State.normal)
-            cells.ImgBtn.image = searchCity[indexPath.row].Img
-            return (cells)
-        }
-        else {
-            let cells=Bundle.main.loadNibNamed("CellsBtn", owner: self, options: nil)?.first as! CellsBtn
-            cells.BtnCities.setTitle(ArrayCity[indexPath.row].City, for:UIControl.State.normal)
-            cells.ImgBtn.image = ArrayCity[indexPath.row].Img
-            return (cells)
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         if searching {
             return searchCity.count
         }
@@ -57,12 +32,56 @@ class ViewController: UIViewController, UITableViewDelegate , UITableViewDataSou
         }
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+
+   func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header=UIView()
+        header.backgroundColor=UIColor.clear
+        return header
+
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        /*let cells=UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "CityCell")
+        cells.textLabel?.text=listcity[indexPath.row]*/
+        if searching {
+            let cells=Bundle.main.loadNibNamed("CellsBtn", owner: self, options: nil)?.first as! CellsBtn
+            cells.BtnCities.setTitle(searchCity[indexPath.section].City, for:UIControl.State.normal)
+            cells.ImgBtn.image = searchCity[indexPath.section].Img
+            return (cells)
+        }
+        else {
+            let cells=Bundle.main.loadNibNamed("CellsBtn", owner: self, options: nil)?.first as! CellsBtn
+            cells.BtnCities.setTitle(ArrayCity[indexPath.section].City, for:UIControl.State.normal)
+            cells.ImgBtn.image = ArrayCity[indexPath.section].Img
+            return (cells)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        if searching {
+//            return searchCity.count
+//        }
+//        else {
+//            return ArrayCity.count
+//        }
+        return 1
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         ArrayCity=[buttonCity(City: "Rome", Img:UIImage(named: "Rome.jpg")),
-        buttonCity(City: "Florence", Img: UIImage(named: "florence.jpg"))]
+        buttonCity(City: "Florence", Img: UIImage(named: "florence.jpg")),
+        buttonCity(City: "Milan", Img: UIImage(named: "milan.png")),
+        buttonCity(City: "Caserta", Img: UIImage(named: "caserta.jpg")),
+        buttonCity(City: "Amsterdam", Img: UIImage(named: "amsterdam.jpg"))]
         
     }
 
