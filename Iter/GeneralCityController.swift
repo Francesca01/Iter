bee463f524d73a1aacb33d0af0cba53e5052d281
Oscar Magarini <oscarmagarini95@icloud.com>
2018-10-17 18:02:17 +0200

    Second Page (almost done)

--- Iter/GeneralCityController.swift
+++ Iter/GeneralCityController.swift
@@ -8,23 +8,55 @@
 
 import UIKit
 
-class GeneralCityController: UIViewController {
+class GeneralCityController: UIViewController, UIScrollViewDelegate{
 
+    @IBOutlet weak var ScrollView: UIScrollView!
+    @IBOutlet weak var PageControl: UIPageControl!
+    var texts: [String] = ["JDIJI", "JIJJ", "fijfidjfie", "fjidjfijfsi"]
+    var frame = CGRect (x:0, y:0, width:0, height:0 )
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.setNavigationBarHidden(false, animated: true)
         // Do any additional setup after loading the view.
+        loadScrollView()
     }
     
+    func loadScrollView() {
+        ScrollView.delegate = self
+        ScrollView.contentSize = CGSize(width: (ScrollView.frame.size.width * CGFloat(texts.count)), height: ScrollView.frame.size.height)
+        
+        PageControl.numberOfPages = texts.count
+        
+        
+        for i in 0..<texts.count{
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//            let textView = UILabel(frame: CGRect(x:
+//                self.ScrollView.frame.size.width * CGFloat(i), y: 0, width:  30 , height: 30))
+//            textView.text = texts[i]
+//            textView.numberOfLines = 0
+//            textView.textColor = UIColor.white
+//            textView.textAlignment = .center
+//            self.ScrollView.addSubview(textView)
+            
+            frame.origin.x = ScrollView.frame.size.width * CGFloat(i)
+            frame.size = ScrollView.frame.size
+            let textView = UITextView (frame: frame)
+            textView.text = texts[i]
+            self.ScrollView.addSubview (textView)
+            textView.backgroundColor=UIColor.clear
+            textView.isEditable = false
+            
+        }
+        
+        ScrollView.contentSize = CGSize (width: (ScrollView.frame.size.width * CGFloat(texts.count)), height: ScrollView.frame.height)
+        ScrollView.delegate = self
     }
-    */
+
+    func scrollViewDidEndDecelerating(_ ScrollView: UIScrollView) {
+        let pageNumber = ScrollView.contentOffset.x / ScrollView.frame.size.width
+        PageControl.currentPage = Int (pageNumber)
+    }
 
 }
