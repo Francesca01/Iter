353dd2a8a78809dcf762d07951a4e80c3fafbf66
Francesca Lettieri <francesca@francescas-macbook-pro.local>
2018-10-18 21:31:51 +0200

    da michele

--- Iter/DaMicheleViewController.swift
+++ Iter/DaMicheleViewController.swift
@@ -7,24 +7,41 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class DaMicheleViewController: UIViewController {
+class DaMicheleViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        mapView.showsUserLocation = true
+        
+        if CLLocationManager.locationServicesEnabled() {
+            if CLLocationManager.authorizationStatus() == .restricted || CLLocationManager.authorizationStatus() == .denied || CLLocationManager.authorizationStatus() == .notDetermined{
+                
+                //        Richiesta Autorizzazione
+                locationManager.requestWhenInUseAuthorization()
+            }
+            locationManager.desiredAccuracy = 1.0
+            locationManager.delegate = self
+            locationManager.startUpdatingLocation()
+        }
+        else {
+            //          Qui potremmo mostrare un alert!!!
+            print("Please, turn on location services or GPS")
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.04, longitudeDelta: 0.04))
+        self.mapView.setRegion(region, animated: true)
     }
-    */
-
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print ("Unable to  access your current location!")
+    }
+    
 }
